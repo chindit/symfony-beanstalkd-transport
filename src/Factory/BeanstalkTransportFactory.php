a78df80e57847f310d03d1baae44a99f6151cf0c
chindit <littletiger58@gmail.com>
2020-06-02 14:52:23 +0200

    Implement TransportFactory constructor

--- src/Factory/BeanstalkTransportFactory.php
+++ src/Factory/BeanstalkTransportFactory.php
@@ -1,13 +1,15 @@
 <?php
 declare(strict_types=1);
 
+use Symfony\Component\Messenger\Transport\Serialization\SerializerInterface;
 use Symfony\Component\Messenger\Transport\TransportFactoryInterface;
+use Symfony\Component\Messenger\Transport\TransportInterface;
 
 class BeanstalkTransportFactory implements TransportFactoryInterface
 {
-	public function createTransport(string $dsn, array $options, \Symfony\Component\Messenger\Transport\Serialization\SerializerInterface $serializer): \Symfony\Component\Messenger\Transport\TransportInterface
+	public function createTransport(string $dsn, array $options, SerializerInterface $serializer): TransportInterface
 	{
-		// TODO: Implement createTransport() method.
+		return new BeanstalkTransport('127.0.0.1', '', $serializer);
 	}
 
 	public function supports(string $dsn, array $options): bool
